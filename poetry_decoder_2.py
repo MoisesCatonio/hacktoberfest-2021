536357b0a140a77126f076006c8e6d2eaf4911ad
MoisesCatonio <moises.catonio@gmail.com>
2021-10-31 21:12:43 -0300

    Divisão de branches e modificações básicas

--- poetry_decoder_2.py
+++ poetry_decoder_2.py
@@ -8,15 +8,15 @@ def decode_first_letter(poem):
         subdivision_key = [subdivision[0].lower()]
         keys.extend(subdivision_key)
 
-    count = 0
+    index = 0
     for key in keys:
-        ordered[key] = poem[count]
-        count += 1 
+        ordered[key] = poem[index]
+        index += 1 
     
     ordered = OrderedDict(sorted(ordered.items()))
     for key in ordered:
         print(ordered[key])
 
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Poema 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("=============================== Poema 2 ===============================")
 decode_first_letter(poem2)
 print("")
